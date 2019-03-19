1afc7ac9d2b8108e690c2278b1f0fa5d6bb012ea
Valery Pavlikov <valeriy.pavlikov@3d4medical.com>
2019-03-19 11:55:14 +0300

    creating new database (postgressql) and updated requirements.txt

--- timetable/settings.py
+++ timetable/settings.py
@@ -68,8 +68,12 @@ WSGI_APPLICATION = 'timetable.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'localhost',
+        'NAME': 'timetable',
+        'PASSWORD': 'timetable',
+        'PORT': 5432,
+        'USER': 'timetable',
     }
 }
 
