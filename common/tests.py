626eda501c900257759a17ab20b02351c1598275
Valery Pavlikov <valeriy.pavlikov@3d4medical.com>
2019-03-19 16:01:07 +0300

    creating new tests

---
+++ common/tests.py
@@ -0,0 +1,22 @@
+from rest_framework.test import APITestCase
+from rest_framework.reverse import reverse as _reverse
+
+from users.factories import UserFactory
+
+
+class BaseAPITestCase(APITestCase):
+    version = 'v1'
+
+    @classmethod
+    def setUpClass(cls):
+        super(BaseAPITestCase, cls).setUpClass()
+        cls.user = UserFactory()
+
+    def setUp(self):
+        self.client.force_login(user=self.user)
+
+    def reverse(self, view_name, args=None, kwargs=None, request=None, format=None, **extra):
+        if kwargs is None:
+            kwargs = {}
+        kwargs.update({'version': self.version})
+        return _reverse(view_name, args, kwargs, request, format, **extra)
