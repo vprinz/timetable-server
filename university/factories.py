739bd3c4b8bd126d94711acb4a5e2fa62c97bad1
Valery Pavlikov <valeriy.pavlikov@3d4medical.com>
2019-04-16 17:05:42 +0300

    updating factories for university

--- university/factories.py
+++ university/factories.py
@@ -1,7 +1,7 @@
 import string
 import factory.fuzzy
 
-from .models import Faculty, Occupation, Group
+from .models import Faculty, Occupation, Group, Subgroup
 
 
 class FacultyFactory(factory.DjangoModelFactory):
@@ -11,19 +11,38 @@ class FacultyFactory(factory.DjangoModelFactory):
     class Meta:
         model = Faculty
 
+    @factory.post_generation
+    def occupations(self, create, extracted, **kwargs):
+        if create and extracted:
+            for extract in extracted:
+                self.occupations.add(extract)
+
 
 class OccupationFactory(factory.DjangoModelFactory):
     title = factory.Sequence(lambda n: 'Occupation {0}'.format(n))
+    short_title = factory.fuzzy.FuzzyText(length=16)
     code = factory.fuzzy.FuzzyText(length=10, chars=string.digits)
 
     class Meta:
         model = Occupation
 
+    @factory.post_generation
+    def groups(self, create, extracted, **kwargs):
+        if create and extracted:
+            for extract in extracted:
+                self.groups.add(extract)
+
 
 class GroupFactory(factory.DjangoModelFactory):
-    title = factory.fuzzy.FuzzyText(length=2, chars=string.digits)
-    faculty = factory.SubFactory(FacultyFactory)
-    occupation = factory.SubFactory(OccupationFactory)
+    number = factory.fuzzy.FuzzyText(length=2, chars=string.digits)
 
     class Meta:
         model = Group
+
+
+class SubgroupFactory(factory.DjangoModelFactory):
+    number = factory.fuzzy.FuzzyText(length=1, chars=string.digits)
+    groups = factory.SubFactory(GroupFactory)
+
+    class Meta:
+        model = Subgroup
