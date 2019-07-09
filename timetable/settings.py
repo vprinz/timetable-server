0184746c37fdda3d2fb3d7e3bbcd6f1fc0d2612c
Valery Pavlikov <valeriy.pavlikov@3d4medical.com>
2019-07-09 19:27:23 +0300

    added STATIC_ROOT

--- timetable/settings.py
+++ timetable/settings.py
@@ -133,6 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'users.User'
