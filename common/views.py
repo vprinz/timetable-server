bf053653f31d0ada23c6a88dcd67a6611481ed6a
Valery Pavlikov <valeriy.pavlikov@3d4medical.com>
2019-07-15 13:17:56 +0300

    changed port for stage server

--- common/views.py
+++ common/views.py
@@ -12,5 +12,5 @@ def home_page(request):
 @login_not_required
 def ws_test(request):
     host = settings.SERVER_FULL_URL.split(':')[0]
-    port = 8000 if settings.DEPLOYMENT_NAME == 'local' else 80
+    port = 8000 if settings.DEPLOYMENT_NAME == 'local' else 8001
     return render(request, 'ws_test.html', {'ws_url': f'ws:{host}:{port}/notifications/'})
