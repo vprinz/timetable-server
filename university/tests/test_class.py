d15d840d978a21d48f4dd5f6d38d424c264eedca
Valery Pavlikov <valeriy.pavlikov@3d4medical.com>
2019-09-27 19:26:16 +0300

    remove sync test for class (cos belongs to timetable)

--- university/tests/test_class.py
+++ university/tests/test_class.py
@@ -1,11 +1,9 @@
 import json
-import time
-from datetime import datetime, timedelta
 
 from rest_framework.status import HTTP_200_OK
 
 from common.tests import BaseAPITestCase
-from ..factories import SubscriptionFactory, TimetableFactory, ClassFactory
+from ..factories import TimetableFactory, ClassFactory
 from ..models import Timetbale, Class, Subscription, Subgroup
 from ..serializers import ClassSerializer
 
@@ -48,32 +46,3 @@ class RestAPISubscription(BaseAPITestCase):
 
         self.assertEqual(response.status_code, HTTP_200_OK)
         self.assertEqual(response.data, list())
-
-    def test_sync(self):
-        subgroup_35_2 = Subgroup.objects.get(group=self.group_35, number='2')
-        SubscriptionFactory(subgroup=subgroup_35_2, user=self.user)
-        timetable_factory_35_2_numerator = TimetableFactory(subgroup=subgroup_35_2, type_of_week=Timetbale.NUMERATOR)
-
-        subscriptions = Subscription.objects.filter(user=self.user)
-        existing_ids = [s.id for s in Timetbale.objects.filter(subgroup__subscription__in=subscriptions)]
-        timestamp = int(datetime.timestamp(datetime.now() + timedelta(seconds=1)))
-        data = {'existing_ids': existing_ids, 'timestamp': timestamp}
-        url = self.reverse('timetables-sync')
-
-        # If nothing changed
-        response = self.client.post(url, json.dumps(data), content_type=self.content_type)
-        self.assertEqual(response.status_code, HTTP_200_OK)
-        self.assertJSONEqual(response.content.decode(), compare_response_with_sync([], []))
-        time.sleep(1)
-
-        # If something changed in timetable
-        timetable_35_1_numerator = Timetbale.objects.get(id=self.timetable.id)
-        timetable_35_1_numerator.save()  # Just save because can't change something
-
-        timetable_35_2_numerator = Timetbale.objects.get(id=timetable_factory_35_2_numerator.id)
-        timetable_35_2_numerator.delete()
-
-        response = self.client.post(url, json.dumps(data), content_type=self.content_type)
-        self.assertEqual(response.status_code, HTTP_200_OK)
-        self.assertJSONEqual(response.content.decode(), compare_response_with_sync(
-            updated_ids=[timetable_35_1_numerator.id], deleted_ids=[timetable_factory_35_2_numerator.id]))
