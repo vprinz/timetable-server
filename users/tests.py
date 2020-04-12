c4bf270c402ccfa4d447eefa7cd24187044b4225
Valery Pavlikov <valeriy.pavlikov@3d4medical.com>
2020-04-12 12:26:31 +0300

    refactoring user's tests

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,5 @@
+import json
+
 from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 
 from common.tests import BaseAPITestCase
@@ -15,53 +17,61 @@ class RestAPIUser(BaseAPITestCase):
         self.user_logout = self.reverse('users-logout')
 
     def test_create_user_with_correct_data(self):
-        response = self.client.post(self.user_create_url, {'email': 'test-email@mail.com', 'password': 'Timetable123'})
+        data = json.dumps({'email': 'test-email@mail.com', 'password': 'Timetable123'})
+        response = self.client.post(self.user_create_url, data=data, content_type=self.content_type)
 
         self.assertEqual(response.status_code, HTTP_201_CREATED)
         self.assertEqual(User.objects.count(), 2)
 
     def test_create_user_with_wrong_pass(self):
-        response = self.client.post(self.user_create_url, {'email': 'test-email@mail.com', 'password': '1597538291'})
+        data = json.dumps({'email': 'test-email@mail.com', 'password': '1597538291'})
+        response = self.client.post(self.user_create_url, data=data, content_type=self.content_type)
 
         self.assertEqual(response.status_code, HTTP_400_BAD_REQUEST)
         self.assertEqual(User.objects.count(), 1)
         self.assertEqual(response.data['password'], ['password_entirely_numeric'])
 
     def test_create_user_with_wrong_email(self):
-        response = self.client.post(self.user_create_url, {'email': 'test-email', 'password': 'Timetable123'})
+        data = json.dumps({'email': 'test-email', 'password': 'Timetable123'})
+        response = self.client.post(self.user_create_url, data=data, content_type=self.content_type)
 
         self.assertEqual(response.status_code, HTTP_400_BAD_REQUEST)
         self.assertEqual(User.objects.count(), 1)
         self.assertEqual(response.data['email'], ['invalid'])
 
     def test_create_user_with_exist_email(self):
+        data = json.dumps({'email': 'exist_email@mail.com', 'password': 'Timetable123'})
         UserFactory(email='exist_email@mail.com')
-        response = self.client.post(self.user_create_url, {'email': 'exist_email@mail.com', 'password': 'Timetable123'})
+        response = self.client.post(self.user_create_url, data=data, content_type=self.content_type)
 
         self.assertEqual(response.status_code, HTTP_400_BAD_REQUEST)
         self.assertEqual(User.objects.count(), 2)
         self.assertEqual(response.data['email'], ['unique'])
 
     def test_create_user_with_no_data(self):
-        response = self.client.post(self.user_create_url, {})
+        data = json.dumps({})
+        response = self.client.post(self.user_create_url, data=data, content_type=self.content_type)
 
         self.assertEqual(response.status_code, HTTP_400_BAD_REQUEST)
         self.assertEqual(User.objects.count(), 1)
         self.assertEqual(response.data['email'], ['required'])
 
     def test_login(self):
-        response = self.client.post(self.user_login_url, data={'email': self.user.email, 'password': 'Timetable123'})
+        data = json.dumps({'email': self.user.email, 'password': 'Timetable123'})
+        response = self.client.post(self.user_login_url, data=data, content_type=self.content_type)
 
         self.assertEqual(response.status_code, HTTP_200_OK)
 
     def test_login_with_wrong_data(self):
-        response = self.client.post(self.user_login_url, data={'email': self.user.email, 'password': 'Timetable'})
+        data = json.dumps({'email': self.user.email, 'password': 'Timetable'})
+        response = self.client.post(self.user_login_url, data=data, content_type=self.content_type)
 
         self.assertEqual(response.status_code, HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data['__all__'], ['incorrect_email_or_password'])
 
     def test_change_user_info(self):
-        response = self.client.patch(self.user_info, data={'first_name': 'Nicholas', 'last_name': 'Barnett'})
+        data = json.dumps({'first_name': 'Nicholas', 'last_name': 'Barnett'})
+        response = self.client.patch(self.user_info, data=data, content_type=self.content_type)
 
         self.assertEqual(User.objects.get(email=self.user.email).get_full_name(), 'Nicholas Barnett')
         self.assertEqual(response.status_code, HTTP_200_OK)
