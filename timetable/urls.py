d10c4d2e0a308e127c0c538ec621164e38bc25b0
Valery Pavlikov <valeriy.pavlikov@3d4medical.com>
2020-05-13 12:24:49 +0300

    flake8-quotes implementing

--- timetable/urls.py
+++ timetable/urls.py
@@ -8,12 +8,12 @@ from common import views
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Snippets API",
+        title='Snippets API',
         default_version='v1',
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
+        description='Test description',
+        terms_of_service='https://www.google.com/policies/terms/',
+        contact=openapi.Contact(email='contact@snippets.local'),
+        license=openapi.License(name='BSD License'),
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
--- users/forms.py
+++ users/forms.py
@@ -5,9 +5,9 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class AuthenticationForm(BaseAuthenticationForm):
-    email = forms.EmailField(label=_("Email"), max_length=75)
-    message_incorrect_email_or_password = _("Адрес электронной почты или пароль введен неверно.")
-    message_inactive = _("Этот аккаунт неактивен.")
+    email = forms.EmailField(label=_('Email'), max_length=75)
+    message_incorrect_email_or_password = _('Адрес электронной почты или пароль введен неверно.')
+    message_inactive = _('Этот аккаунт неактивен.')
 
     def __init__(self, request=None, *args, **kwargs):
         super(AuthenticationForm, self).__init__(request, *args, **kwargs)
